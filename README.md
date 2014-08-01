ea76fab97fd529a3e8a9b2ab23b1584e36a1906e
Kesav Kumar Kolla <kesavkolla@gmail.com>
2014-07-31 23:07:41 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BHUtils
+
+This project holds all the command line utilities for interacting with bullhorn
---
+++ src/main/java/com/$314e/bullhorn/MassUpdater.java
@@ -0,0 +1,25 @@
+package com.$314e.bullhorn;
+
+import java.util.Iterator;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.DefaultConfigurationBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MassUpdater {
+	private static Logger logger = LogManager.getLogger(MassUpdater.class);
+	private static Configuration config;
+
+	public static void main(final String... args) throws Exception {
+		final DefaultConfigurationBuilder factory = new DefaultConfigurationBuilder("META-INF/config.xml");
+		config = factory.getConfiguration();
+		for (final Iterator<String> ite = config.getKeys(); ite.hasNext();) {
+			final String key = ite.next();
+			logger.debug("{}: {}", key, config.getProperty(key));
+		}
+		logger.entry();
+		logger.error("Hello {}", "kesav");
+		logger.exit();
+	}
+}
