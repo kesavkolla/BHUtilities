0e72390cda43801db67c641b3708aa00641c04f7
Kesav Kolla <kesavkolla@hotmail.com>
2014-08-27 14:29:46 -0700

    Change to SQL Server properties

--- src/main/java/com/$314e/bullhorn/JobActivityReport.java
+++ src/main/java/com/$314e/bullhorn/JobActivityReport.java
@@ -1,5 +1,14 @@
 package com.$314e.bullhorn;
 
+import java.io.BufferedWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.time.format.DateTimeFormatter;
 import java.util.Iterator;
 
 import org.apache.logging.log4j.LogManager;
@@ -20,6 +29,8 @@ public class JobActivityReport extends BaseUtil {
 
 	private void doExecute() throws Exception {
 		LOGGER.entry();
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
 		final ObjectNode recruiters = getEntityApi().query(BHRestApi.Entity.ENTITY_TYPE.PERSON, getRestToken(),
 				"isDeleted=false AND 93 MEMBER OF distributionLists AND firstName NOT IN ('Alok', 'Abhishek')",
 				"id,name,email", "+id", 500, 0);
@@ -31,6 +42,18 @@ public class JobActivityReport extends BaseUtil {
 			final int recruiterId = recruiter.path("id").asInt();
 
 		}
+		final StringBuilder query = new StringBuilder();
+		query.append("SELECT CURRENT_USER, CURRENT_TIMESTAMP");
+		try (final Connection con = DriverManager.getConnection(appConfig.getString("BH_DATAMART_URL"),
+				appConfig.getString("BH_DATAMART_USER"), appConfig.getString("BH_DATAMART_PASSWORD"));
+				final Statement stmt = con.createStatement();
+				final ResultSet rs = stmt.executeQuery(query.toString());) {
+			while (rs.next()) {
+				LOGGER.debug("I'm: {}    Today: {}", rs.getString(1),
+						rs.getTimestamp(2).toLocalDateTime().format(DateTimeFormatter.ISO_DATE_TIME));
+			}
+		}
+
 		LOGGER.exit();
 	}
 
