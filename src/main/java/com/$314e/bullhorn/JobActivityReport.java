6cfcd07dcadebee7f620bb3fa59775aa99e3db44
Kesav Kumar Kolla <kesavkolla@gmail.com>
2014-08-29 09:24:42 -0700

    Added more code for report

--- src/main/java/com/$314e/bullhorn/JobActivityReport.java
+++ src/main/java/com/$314e/bullhorn/JobActivityReport.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Iterator;
 import java.util.Spliterator;
 import java.util.Spliterators;
@@ -31,6 +34,52 @@ public class JobActivityReport extends BaseUtil {
 	private void doExecute() throws Exception {
 		LOGGER.entry();
 
+		LocalDate periodStart = LocalDate.now();
+		try {
+			periodStart = LocalDate
+					.parse(appConfig.getString("periodStart"), DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+		} catch (final Throwable th) {
+		}
+		LocalDate periodEnd = LocalDate.now();
+		try {
+			periodEnd = LocalDate.parse(appConfig.getString("periodEnd"), DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+		} catch (final Throwable th) {
+		}
+		final String strPeriodStart = periodStart.atTime(LocalTime.MIDNIGHT).format(DateTimeFormatter.ISO_DATE_TIME)
+				.replace('T', ' ');
+		final String strPeriodEnd = periodEnd.atTime(23, 59, 59).format(DateTimeFormatter.ISO_DATE_TIME)
+				.replace('T', ' ');
+		LOGGER.debug("Period Start: {}     Period End: {}", strPeriodStart, strPeriodEnd);
+
+		final StringBuilder allactivityQuery = new StringBuilder(1024 * 2);
+		// Internal Submission
+		allactivityQuery.append("select * from ( ");
+		allactivityQuery.append("select 'Internal Submission' as activity, count(*) as cnt from dbo.JobSubmission")
+				.append(" where dateAdded between {ts '").append(strPeriodStart).append("'} and {ts '")
+				.append(strPeriodEnd).append("'} and sendingUserID={userID}");
+		// Client Submission
+		allactivityQuery
+				.append(" union all ")
+				.append("select 'Client Submission', count(*) from dbo.Sendout ")
+				.append(" inner join dbo.JobSubmission on Sendout.jobOrderID = JobSubmission.jobOrderID and Sendout.candidateID = JobSubmission.candidateID")
+				.append(" where Sendout.dateAdded between {ts '").append(strPeriodStart).append("'} and {ts '")
+				.append(strPeriodEnd).append("'}").append("  and JobSubmission.sendingUserID={userID}");
+		// Interviews
+		allactivityQuery
+				.append(" union all ")
+				.append("select 'Interview', count(*) from dbo.Appointment")
+				.append(" inner join dbo.JobSubmission on Appointment.jobOrderID = JobSubmission.jobOrderID and Appointment.candidateReferenceID = JobSubmission.candidateID")
+				.append(" where dbo.Appointment.dateAdded between {ts '").append(strPeriodStart).append("'} and {ts '")
+				.append(strPeriodEnd).append("'}").append(" and JobSubmission.sendingUserID={userID}");
+
+		// Notes
+		allactivityQuery.append(" union all ").append("select Note.action+'_Note', count(*) from dbo.NoteEntity ")
+				.append(" inner join dbo.Note on NoteEntity.noteID = Note.noteID")
+				.append(" where entityName='JobOrder'").append(" and  Note.dateAdded  between {ts '")
+				.append(strPeriodStart).append("'} and {ts '").append(strPeriodEnd).append("'}")
+				.append(" and Note.commentingPersonID={userID}").append(" group by Note.action");
+		allactivityQuery.append(" ) tmp where cnt > 0");
+
 		// Get all the recruiters from Bullhorn
 		final ObjectNode recruiters = getEntityApi().query(BHRestApi.Entity.ENTITY_TYPE.PERSON, getRestToken(),
 				"isDeleted=false AND 93 MEMBER OF distributionLists AND firstName NOT IN ('Alok', 'Abhishek')",
@@ -47,37 +96,16 @@ public class JobActivityReport extends BaseUtil {
 				final JsonNode recruiter = iter.next();
 				LOGGER.debug(recruiter);
 
-				final int recruiterId = recruiter.path("id").asInt();
-
-				final StringBuilder jobIdsQuery = new StringBuilder();
-				jobIdsQuery
-						.append("select JobOrder.jobOrderID, JobOrder.title, JobOrder.status, JobOrder.isOpen")
-						.append(" from dbo.JobOrder")
-						.append(" inner join dbo.JobOrderAssignedUsers on JobOrder.jobOrderID = JobOrderAssignedUsers.jobOrderID")
-						.append(" and JobOrderAssignedUsers.isDeleted = 0").append(" where JobOrder.isDeleted = 0")
-						.append(" and (JobOrder.ownerID = ").append(recruiterId)
-						.append(" or JobOrderAssignedUsers.userID = ").append(recruiterId)
-						.append(" or correlatedCustomText2 like '%").append(recruiter.path("name").asText().trim())
-						.append("%' )");
-				LOGGER.debug(jobIdsQuery);
-				final ArrayNode jobOrders = JsonNodeFactory.instance.arrayNode();
-				try (final ResultSet rs = stmt.executeQuery(jobIdsQuery.toString());) {
+				final String query = allactivityQuery.toString().replace("{userID}", recruiter.path("id").asText());
+				LOGGER.debug(query);
+				final ObjectNode recruiterData = JsonNodeFactory.instance.objectNode();
+				recruiterData.put("name", recruiter.path("name").asText());
+				try (final ResultSet rs = stmt.executeQuery(query);) {
 					while (rs.next()) {
-						// @formatter:off
-						jobOrders.addObject()
-							.put("jobOrderID", rs.getInt("jobOrderID"))
-							.put("title", rs.getString("title"))
-							.put("status", rs.getString("status"))
-							.put("isOpen", rs.getInt("isOpen"));
-						// @formatter:on
+						recruiterData.with("allactivity").put(rs.getString(1), rs.getInt(2));
 					}
 				}
-				LOGGER.debug(jobOrders);
-				LOGGER.debug(getJobOrderIds(jobOrders));
-
-				if (cnt++ > 0) {
-					break;
-				}
+				LOGGER.debug(recruiterData);
 			}
 		}
 
